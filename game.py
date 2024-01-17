04ad04b80659ec0855edfed22e1a5b1811973745
Jay <hornberger126@gmail.com>
2024-01-16 21:45:50 -0500

    TileMapStarted Commit

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
-
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 class Game:
     def __init__(self):
         pygame.init()
@@ -10,21 +12,39 @@ class Game:
         icon = pygame.image.load('Icon.png') 
         pygame.display.set_icon(icon)
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
         #Sets FPS at 60
         self.clock = pygame.time.Clock()
 
-        #Loads Img on screen
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        self.img.set_colorkey((0, 0, 0))
-        self.img_pos = [160, 260]
+       #Movement
         self.movement = [False, False]
+
+        #Load assets
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png')
+        }
+
+        #Spawns Player
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+
+        #Loads TileMap
+        self.tilemap = Tilemap(self, tile_size=16)
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
-            self.img_pos[1] += self.movement[1] - self.movement[0]
-            self.screen.blit(self.img, self.img_pos)
+            self.display.fill((14, 219, 248))
+
+            #Renders behind player
+            self.tilemap.render(self.display)
+            
+            #Player Movemet
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
 
             #Exits the window
             for event in pygame.event.get():
@@ -32,18 +52,19 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-                #Moves Item
+                #Moves Player
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.movement[0] = True
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_d:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.movement[0] = False
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_d:
                         self.movement[1] = False
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
